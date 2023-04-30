dce15823865e1afa0847538110f182b338dbb845
yaneyalvarado <alvyaney@gmail.com>
2023-04-30 14:37:19 -0600

    added the required prompted msgs

--- Shape.js
+++ Shape.js
@@ -16,8 +16,17 @@ return ` <circle cx="150" cy="100" r="80" fill="${this.color}" />`
     }
 }
 
+class Triangle extends Shape {
+    render() {
+return `<triangle cx="150" cy="100" r="80" fill="${this.color}" />`
+    }
+}
+
+class Square extends Shape {
+    render() {
+return `<square cx="150" cy="100" r="80" fill="${this.color}" />`
+    }
+}
 
 
-
-
-module.exports = {Circle}
+module.exports = {Circle, Triangle, Square}
--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const inquirer = require("inquirer")
 const {Circle} = require("./Shape")
-
-
+const {Triangle} = require("./Shape")
+const {Square} = require("./Shape")
 
 inquirer
   .prompt([
     {
       type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
-    }
-   
+      message: 'Enter up to three characters',
+      name: 'text',
+    },
+    {
+        type: 'input',
+        message: 'Enter the text color',
+        name: 'text color',
+      },
+      {
+        type: 'list',
+        message: 'Choose a shape',
+        choices: ['Circle', 'Triangle', 'Square'],
+        name: 'shape'
+      },
+      {
+        type: 'input',
+        message: 'Choose the color for your shaoe',
+        name: 'shape color',
+      } 
   ])
   .then((response) => {
     let shape = new Circle() 
@@ -19,4 +34,3 @@ inquirer
   }
   );
 
-  
